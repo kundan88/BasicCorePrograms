98293f2673ac12899476ce572b42fec8526b4d5c
Kundan <kundan kamble>
2022-07-30 13:02:36 -0800

    Added Leap Year program

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class LeapYear
+    {
+        public static void leapYear()
+        {
+             int y;
+             Console.WriteLine("Enter the Year in Four Digits : ");
+                y = Convert.ToInt32(Console.ReadLine());
+               if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+                {
+                    Console.WriteLine("{0} is a Leap Year", y);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a Leap Year", y);
+                }
+                Console.ReadLine();
+            }
+            
+
+        }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,9 +7,7 @@ namespace BasicCorePrograms
     {
         public static void Main(string[] args)
         {
-            FlipCoin.CalFlipCoin();
-            FlipCoin.DisplayFlipCoin();
-           
+            LeapYear.leapYear();
         }
     }
 }
