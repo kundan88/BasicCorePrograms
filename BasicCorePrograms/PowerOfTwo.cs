0b120e6f5c980e33205ad582a99ba74b253ed16a
Kundan <kundan kamble>
2022-07-30 13:10:41 -0800

    Added Power Of Two program

---
+++ BasicCorePrograms/PowerOfTwo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class PowerOfTwo
+    {
+        public static void powerOfTwo()
+        {
+            int n;
+            Console.WriteLine("Enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (0 <= n && n < 31)
+            {
+                Console.WriteLine("The Power of 2 are:");
+                for (int i = 0; i <= n; i++)
+                {
+                    Console.Write("");
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Num can't take the power of 2 as bigger than 31, Enter Less Than 31");
+            }
+        }
+    }
+}
