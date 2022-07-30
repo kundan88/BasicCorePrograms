7f6fa1eea166e736029700e1873aa09b51eda9ad
Kundan <kundan kamble>
2022-07-30 13:36:10 -0800

    Added Prime Factor program

---
+++ BasicCorePrograms/PrimeFactor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class PrimeFactor
+    {
+        public static void Factor()
+        {
+               int n, i;
+                Console.WriteLine("Enter the Number:");
+                n = int.Parse(Console.ReadLine());
+            
+                Console.WriteLine("Factors:");
+                for (i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
